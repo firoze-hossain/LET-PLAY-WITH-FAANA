e0ae9e47b8690218be2bc5093c520b51a1bba7da
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-05-22 23:59:15 +0600

    escape sequence

--- src/com/roze/faana/HelloFAANA.java
+++
@@ -1,7 +0,0 @@
-package com.roze.faana;
-
-public class HelloFAANA {
-    public static void main(String[] args) {
-        System.out.println("Hello FAANA");
-    }
-}
---
+++ src/com/roze/faana/java/escapeSequence/EscapeSequence.java
@@ -0,0 +1,21 @@
+package com.roze.faana.java.escapeSequence;
+
+public class EscapeSequence {
+    public static void main(String[] args) {
+        //new line-(backslash- \n)
+        System.out.print("Hello\n");
+        System.out.println("Java");
+
+       //for tab(backslash-\t)-more spaces
+        System.out.print("\tWelcome\n");
+        System.out.println("In Depth");
+
+        //print text in double quotes
+        System.out.print("\"Spring\"\n");
+        System.out.println("Boot");
+
+        //want too print backslash
+        System.out.print("\\Firoze\n");
+        System.out.println("Hossain");
+    }
+}
