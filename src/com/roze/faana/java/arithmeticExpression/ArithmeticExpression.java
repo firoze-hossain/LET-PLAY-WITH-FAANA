e2e122ee52acfdbc436cdb7a5f060ba7ba066872
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-05-29 12:48:34 +0600

    arithmetic expression

---
+++ src/com/roze/faana/java/arithmeticExpression/ArithmeticExpression.java
@@ -0,0 +1,26 @@
+package com.roze.faana.java.arithmeticExpression;
+
+public class ArithmeticExpression {
+    public static void main(String[] args) {
+        int num = 10;
+        //addition
+        num = num + 1;
+        //increment by 1
+        num++;
+        //subtraction
+        num = num - 2;
+        //multiplication
+        num = num * 2;
+        //division
+        num = num / 2;
+        //modulus
+        // num=num%3;
+        System.out.println(num);
+
+        //casting
+
+        double num1 = (double) num / 3;
+        System.out.println(num1);
+
+    }
+}
