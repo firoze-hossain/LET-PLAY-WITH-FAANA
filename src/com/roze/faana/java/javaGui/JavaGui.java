73c7b052bc1ebcf9f9658c5bc164fa50d2f1368c
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-06-01 12:26:45 +0600

    java gui intro

---
+++ src/com/roze/faana/java/javaGui/JavaGui.java
@@ -0,0 +1,14 @@
+package com.roze.faana.java.javaGui;
+
+import javax.swing.*;
+
+public class JavaGui {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "Your age is " + age + " years old");
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "Your height is " + height + " cm tall");
+    }
+}
