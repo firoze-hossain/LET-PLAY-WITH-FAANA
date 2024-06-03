7dfd3a32e0ba6d6bb94957089936c9f50bd9417e
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-06-03 19:49:12 +0600

    for loop

---
+++ src/com/roze/faana/java/forLoop/ForLoop.java
@@ -0,0 +1,18 @@
+package com.roze.faana.java.forLoop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("********************");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("********************");
+        for (int i = 0; i < 20; i += 2) {
+            System.out.println(i);
+        }
+        System.out.println("Welcome back Firoze");
+    }
+}
