3a463c6d3dc383e65ca890a610f6fe735e1293db
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-06-15 18:32:40 +0600

    final keyword in java

---
+++ src/com/roze/faana/java/finalKeyword/FinalKeyword.java
@@ -0,0 +1,8 @@
+package com.roze.faana.java.finalKeyword;
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+        final double X = 21.5648;
+        System.out.println(X);
+    }
+}
