8df96fcec7124fd43cc79890842ac693bf624cdd
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-07-03 21:59:26 +0600

    super keyword

---
+++ src/com/roze/faana/java/superKeyword/Main.java
@@ -0,0 +1,8 @@
+package com.roze.faana.java.superKeyword;
+
+public class Main {
+    public static void main(String[] args) {
+        Hero hero = new Hero("Superman", 100, "Strong natural power");
+        System.out.println(hero);
+    }
+}
