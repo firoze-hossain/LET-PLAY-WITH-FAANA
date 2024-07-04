665922908bee2f31edc1ac84cd58ae75c5f2fa45
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-07-04 23:09:19 +0600

    abstraction

---
+++ src/com/roze/faana/java/abstraction/Car.java
@@ -0,0 +1,8 @@
+package com.roze.faana.java.abstraction;
+
+public class Car extends Vehicle {
+    @Override
+    void go() {
+        System.out.println("The driver is driving the car");
+    }
+}
---
+++ src/com/roze/faana/java/abstraction/Vehicle.java
@@ -0,0 +1,5 @@
+package com.roze.faana.java.abstraction;
+
+public abstract class Vehicle {
+    abstract void go();
+}
